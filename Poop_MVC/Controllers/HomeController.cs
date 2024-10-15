e1e62ab73af1b129f75ef8043130a74cc608e356
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2024-10-15 14:52:00 -0400

    Add HttpPost with logic to check if  InputWord  IsPalindrome

--- Poop_MVC/Controllers/HomeController.cs
+++ Poop_MVC/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-using Poop_MVC.Models;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
+using Poop_MVC.Models;
 
 namespace Poop_MVC.Controllers
 {
@@ -18,6 +19,45 @@ namespace Poop_MVC.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Reverse()
+        {
+            PalindromeModel model = new();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Reverse(PalindromeModel palindrome)
+        {
+            string inputWord = palindrome.InputWord;
+
+            string revWord = "";
+
+            for (int i = inputWord.Length - 1; i >= 0; i--)
+            {
+                revWord += inputWord[i];
+            }
+
+            palindrome.RevWord = revWord;
+
+            revWord = Regex.Replace(revWord.ToLower(),"[^a-zA-Z0-9]+","" );
+            inputWord = Regex.Replace(inputWord.ToLower(),"[^a-zA-Z0-9]+","");
+
+            if (revWord == inputWord)
+            {
+                palindrome.IsPalindrome = true;
+                palindrome.Message = $"Success '{palindrome.InputWord}' is a Palindrome";
+            }
+            else
+            {
+                palindrome.IsPalindrome = false;
+                palindrome.Message = $"Sorry, '{palindrome.InputWord}' is not a Palindrome";
+            }
+
+            return View(palindrome);
+        }
         public IActionResult Privacy()
         {
             return View();
