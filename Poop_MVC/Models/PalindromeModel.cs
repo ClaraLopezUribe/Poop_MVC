a52356407aefab1a58707d8512ee9cde056367db
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2024-10-15 14:48:58 -0400

    Add PalindromeModel and Reverse page view.

---
+++ Poop_MVC/Models/PalindromeModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Poop_MVC.Models
+{
+    public class PalindromeModel
+    {
+        public string InputWord { get; set; }
+
+        public string RevWord { get; set; }
+
+        public bool IsPalindrome { get; set; }
+        public string Message { get; set; }
+    }
+}
